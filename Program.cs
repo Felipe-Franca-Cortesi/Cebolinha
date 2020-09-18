f49898ea8576a32ba49f56b53eb85cff2768df53
Felipe França <contatofelipefranca12@gmail.com>
2020-09-17 22:03:57 -0300

    Adiciona frase

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Digite uma frase: ");
+            string frase = Console.ReadLine().Replace("r", "l").Replace("R", "L");
+            Console.WriteLine($"Como o cebolinha falaria: {frase}");
+            Console.ReadKey();
+
+        }
+    }
+}
